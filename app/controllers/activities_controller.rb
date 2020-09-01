21f4d5a84105e5891f35f804f17c62eefb71644d
joannabpark <joannabpark1@gmail.com>
2020-09-01 11:14:31 -0500

    done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,23 @@ class ActivitiesController < ApplicationController
   def index
     @activities = Activity.all
   end
+
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.create(activity_params)
+    redirect_to activity_path(@activity)
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :difficulty)
+  end
 end
